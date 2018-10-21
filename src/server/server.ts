518f07f03eb4e97d715c7b613a008bfaedd8bc08
Matthew Rodgers (macbook) <mdrodge@g.clemson.edu>
2018-10-21 15:42:01 -0700

    still not serving client correctly

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,20 @@
 const Koa = require('koa');
-const KoaStatic = require('koa-static');
+const Router = require('koa-router');
+const send = require('koa-send');
+const path = require('path');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(KoaStatic('../client/index.html'));
+const publicPath = path.join(__dirname, '..', '..', 'public');
+
+router.get('**', async (ctx: any, next: Function) => {
+  console.log('sending');
+  await send(ctx, '/Users/mrod/code/cooking-companion/public/index.html');
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(3000);
